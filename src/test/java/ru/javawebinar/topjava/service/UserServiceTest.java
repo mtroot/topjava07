28f24a3647fffbe708686174fd67be6e0c05d8c6
Timur Muratov <timur_izhevsk@mail.ru>
2016-07-21 17:48:13 +0300

    HW05 add tests by profiles

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -28,8 +28,8 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles({Profiles.ACTIVE_DB, Profiles.DATAJPA})
-public class UserServiceTest {
+@ActiveProfiles(Profiles.ACTIVE_DB)
+public abstract class UserServiceTest {
 
     @Autowired
     protected UserService service;
